738927390a387a2a563d6a154182ad5baa35c314
Bilal Almazbekov <138653003+bilyashik@users.noreply.github.com>
2023-07-14 07:43:51 +0600

    Add files via upload

---
+++ 2_5282742635151240615/project_program_for_students-master/js/lesson.js
@@ -0,0 +1,17 @@
+const phoneInput = document.querySelector('#phone_input')
+const phoneCheck = document.querySelector('#phone_button')
+const phoneResult = document.querySelector('#phone_result')
+
+const regExp = /^\+996 \d{3} \d{2}-\d{2}-\d{2}$/
+
+phoneInput.value = '+996 '
+
+phoneCheck.addEventListener('click', () => {
+    if (regExp.test(phoneInput.value)) {
+        phoneResult.innerHTML = 'OK'
+        phoneResult.style.color = 'green'
+    } else {
+        phoneResult.innerHTML = ' NOT OK'
+        phoneResult.style.color = 'red'
+    }
+})
